f9c370fb04579a53fa65575410ecd43000037589
Ted Kaemming <ted@kaemming.com>
2012-11-20 18:04:48 -0800

    update scripts

--- getarcanist.sh
+++ getarcanist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
-curl -L https://raw.github.com/disqus/disqus-arcanist/master/installarcanist.sh -o /tmp/installarcanist.sh || exit 1
-/bin/sh /tmp/installarcanist.sh || exit 1
+curl -L https://raw.github.com/disqus/disqus-arcanist/master/installarcanist.sh -o /tmp/installarcanist.sh
+/bin/bash /tmp/installarcanist.sh
--- installarcanist.sh
+++ installarcanist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 # Downloads arcanist, libphutil, etc and configures your system
 
@@ -6,51 +7,47 @@ LOC_DIR="/usr/local"
 BIN_DIR="$LOC_DIR/bin"
 PHP_DIR="$LOC_DIR/include/php"
 
-if [ ! -w "$BIN_DIR" ] || [ ! -w "$PHP_DIR" ]; then
+if [ ! -w "$LOC_DIR" ]; then
     if [ -z "$SUDO_USER" ]; then
-        echo "Re-running installation with sudo (no permission on /usr/local for current user)."
-        if [ `uname` == "Darwin" ]; then
-            exec sudo /bin/bash $0 $*
-        else
-            exec sudo $0 $*
-        fi;
+        echo "Re-running installation with sudo (no permission on $LOC_DIR for current user)."
+        exec sudo /bin/sh $0 $*
     else
-        echo "We can't seem to access /usr/local. Please check permissions on this folder and try again."
+        echo "We can't seem to access ${LOC_DIR}. Please check permissions on this folder and try again."
         exit -1
     fi;
 fi;
 
 if [ ! -e "$PHP_DIR" ]; then
-    mkdir -p $PHP_DIR || exit -1
+    mkdir -p $PHP_DIR
 fi;
 
 # Install or update libphutil
 echo "Updating libphutil.."
 if [ -e "$PHP_DIR/libphutil" ]; then
-    arc upgrade || exit -1
+    arc upgrade
 else
-    git clone git://github.com/facebook/libphutil.git "$PHP_DIR/libphutil" || exit -1
-    git clone git://github.com/facebook/arcanist.git "$PHP_DIR/arcanist" || exit -1
-    git clone git://github.com/facebook/phabricator.git "$PHP_DIR/phabricator" || exit -1
+    git clone git://github.com/facebook/libphutil.git "$PHP_DIR/libphutil"
+    git clone git://github.com/facebook/arcanist.git "$PHP_DIR/arcanist"
+    git clone git://github.com/facebook/phabricator.git "$PHP_DIR/phabricator"
 fi
 
 # Install or update libdisqus
 echo "Updating libdisqus.."
 if [ -e "$PHP_DIR/libdisqus" ]; then
-    cd "$PHP_DIR/libdisqus" && git pull origin master || exit -1
+    cd "$PHP_DIR/libdisqus" && git pull origin master
 else
-    git clone git://github.com/disqus/disqus-arcanist.git "$PHP_DIR/libdisqus" || exit -1
+    git clone git://github.com/disqus/disqus-arcanist.git "$PHP_DIR/libdisqus"
 fi
 
 # Register arc commands
 echo "Registering arc commands.."
 
 ## create-arcconfig
-ln -fs "$PHP_DIR/libdisqus/bin/create-arcconfig" "$BIN_DIR/create-arcconfig" || exit -1
+ln -fs "$PHP_DIR/libdisqus/bin/create-arcconfig" "$BIN_DIR/create-arcconfig"
 chmod +x "$BIN_DIR/create-arcconfig"
 
 ## update-arcanist
-ln -fs "$PHP_DIR/libdisqus/bin/update-arcanist" "$BIN_DIR/update-arcanist" || exit -1
+ln -fs "$PHP_DIR/libdisqus/bin/update-arcanist" "$BIN_DIR/update-arcanist"
 chmod +x "$BIN_DIR/update-arcanist"
 
 ## arc
