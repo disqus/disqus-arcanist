d6f2f4d90ce8ed8efbc5d2c8affeeb648c9213e7
Adam Hitchcock <adam@northisup.com>
2014-04-10 11:01:21 -0700

    only look in the tests folder by default
    Summary: ^^
    
    Test Plan: none
    
    Reviewers: jeff
    
    Differential Revision: http://phabricator.local.disqus.net/D10931

--- src/unittest/SetupPyUnitTestEngine.php
+++ src/unittest/SetupPyUnitTestEngine.php
@@ -7,7 +7,7 @@ final class SetupPyUnitTestEngine extends PythonBaseUnitTestEngine {
         $working_copy = $this->getWorkingCopy();
         return $working_copy->getConfig(
             "unit.engine.setup_py_args",
-            "nosetests --with-xunit --xunit-file=test_results/nosetests.xml"
+            "nosetests --with-xunit --xunit-file=test_results/nosetests.xml --tests=tests"
         );
     }
 }
